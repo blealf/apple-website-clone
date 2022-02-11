f10fce3d621554ae0976677933aa5540af622e9e
BLESSING ALFRED <blessing.alfred@mypatricia.co>
2022-02-11 23:20:56 +0100

    added products page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/header/Header.jsx'
 import Advert from './components/header/Advert.jsx'
+import Products from './components/product/Products.jsx'
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Advert/>
+      <Products/>
     </div>
   );
 }
